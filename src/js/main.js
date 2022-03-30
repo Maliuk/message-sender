341c0deac1b20d4d0ececb1d111dbbeed6ee5e57
maliuk <Od2od985!>
2022-03-31 02:50:38 +0300

    Add Twilio SMS service.

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,15 @@ const { dialog } = require('electron');
 const fs = require('fs');
 const readline = require('readline');
 const nodemailer = require('nodemailer');
+const twilioClient = require("twilio");
+const Store = require('electron-store');
+const {phone} = require('phone');
+const emailValidator = require("email-validator");
 
 //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-var isStarted = false;
+const store = new Store();
+let isStarted = false;
 
 const message = {
     error: message => {
@@ -22,29 +27,40 @@ const message = {
     }
 };
 
-const createWindow = () => {
+const createWindow = (file = 'index.html', width = 800, height = 700, parent = null, isShown = true) => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 700,
+        width: width,
+        height: height,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         },
         resizable: false,
-        maximizable: false
-    })
+        maximizable: false,
+        show: isShown
+    });
 
-    win.loadFile('index.html');
+    win.loadFile(file);
 
-    //win.webContents.openDevTools();
+    win.setParentWindow(parent);
+
+    /*win.webContents.openDevTools({
+        mode: 'undocked'
+    });*/
 
     return win;
 }
 
 app.whenReady().then(() => {
     let win = createWindow();
+    let winSettings = createWindow('setting.html', 500, 700, win, false)
+        .on('close', function (e) {
+            e.preventDefault();
+            this.hide();
+        });
 
-    console.log("App is started");
+    console.log('Settings storage: ', app.getPath('userData'));
+    console.log('Settings: ', store.get('settings'));
 
     ipc.on('fromDataMessage', (event, data) => {
         console.log(data);
@@ -100,15 +116,45 @@ app.whenReady().then(() => {
         start(false);
         event.sender.send('asyncReply', message.error("Stopped by user."));
     });
+
+    ipc.on("showSettings", (event, data) => {
+        winSettings.reload();
+        winSettings.show();
+    });
+
+    ipc.on("saveSettings", (event, data) => {
+        winSettings.hide();
+        event.sender.send('asyncReply', message.success("Settings saved"));
+    });
 });
 
 async function processLineByLine(file, win, data) {
+    const settings = store.get('settings');
+
+    let sendSMS;
+    let sendEmail = sendSMS = false;
+
+    if (settings && settings.smtpServer && settings.smtpPort && settings.smtpLogin && settings.smtpPassword) {
+        sendEmail = true;
+    }
+
+    if (settings && settings.accountSid && settings.authToken && settings.phoneFrom) {
+        sendSMS = true;
+    }
+
+    if ( ! sendSMS && ! sendEmail) {
+        win.webContents.send('asyncReply', message.error("Not fully completed settings"));
+        return;
+    }
+
+    const tClient = new twilioClient(settings.accountSid, settings.authToken);
+
     const transporter = nodemailer.createTransport({
-        host: data['smtpServer'].trim(),
-        port: data['smtpPort'].trim(),
+        host: settings.smtpServer.trim(),
+        port: settings.smtpPort.trim(),
         auth: {
-            user: data['smtpLogin'].trim(),
-            pass: data['smtpPassword'],
+            user: settings.smtpLogin.trim(),
+            pass: settings.smtpPassword,
         },
         tls: {
             rejectUnauthorized: false
@@ -128,22 +174,41 @@ async function processLineByLine(file, win, data) {
         if ( ! isStarted)
             return;
 
+        const checkPhone = phone(emailTo);
+
         try {
-            await transporter.sendMail({
-                from: data['emailFrom'].trim(),
-                to: emailTo,
-                subject: data['subject'],
-                text: data['message'],
-            }).then(info => {
-                win.webContents.send('asyncReply', message.success(`Email sent successfully to ${emailTo}`));
-            }).catch(error => {
-                win.webContents.send('asyncReply', message.error(error));
-            });
+            if (checkPhone.isValid && sendSMS) {
+                await tClient.messages
+                    .create({
+                        body: data['message'],
+                        from: settings.phoneFrom,
+                        to: emailTo
+                    })
+                    .then(messageId => win.webContents.send('asyncReply', message.success(`SMS sent successfully to ${emailTo}`)))
+                    .catch(error => win.webContents.send('asyncReply', message.error(`SMS send error to number: ${emailTo} - ${error}`)));
+            }
+            else if (emailValidator.validate(emailTo) && sendEmail) {
+                await transporter.sendMail({
+                    from: data['emailFrom'].trim(),
+                    to: emailTo,
+                    subject: data['subject'],
+                    text: data['message'],
+                }).then(info => {
+                    win.webContents.send('asyncReply', message.success(`Email sent successfully to ${emailTo}`));
+                }).catch(error => {
+                    win.webContents.send('asyncReply', message.error(`Email send error to email: ${emailTo} - ${error}`));
+                });
+            }
+            else {
+                win.webContents.send('asyncReply', message.error(`Oops!!! I can't send email or SMS to this row: ${emailTo}`));
+            }
         }
         catch (error) {
             win.webContents.send('asyncReply', message.error(error));
             return;
         }
+
+        await sleep(170);
     }
 }
 
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,36 +1,80 @@
 const electron = require('electron');
 const ipc = require('electron').ipcRenderer;
 const {dialog} = require('electron');
+const twilioClient = require("twilio");
+const Store = require('electron-store');
 
 console.log("App is started");
 
 (function () {
+    const store = new Store();
+
     let messageForm = document.getElementById('mail-form'),
+        settingsForm = document.getElementById('settings-form'),
         logs = document.getElementById('logs'),
-        startButton = document.getElementById('start'),
-        stopButton = document.getElementById('stop');
+        settingsButton = document.getElementById('settings'),
+        stopButton = document.getElementById('stop'),
+        settings = store.get('settings');
 
-    stopButton.addEventListener('click', function (e) {
-        ipc.send("stopMessage");
-    });
+    if (settings && typeof settings == "object")
+        for (let key in settings) {
+            let field = document.getElementById(key);
+            if (field) {
+                field.value = settings[key];
+            }
+        }
 
-    messageForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        const data = new FormData(this);
-        let object = [];
-
-        data.forEach((value, key) => {
-            object[key] = value;
+    if (stopButton)
+        stopButton.addEventListener('click', function (e) {
+            ipc.send("stopMessage");
         });
 
-        ipc.send("fromDataMessage", object);
-        ipc.send("startMessage");
+    if (messageForm)
+        messageForm.addEventListener('submit', function (e) {
+            e.preventDefault();
 
-        return false;
-    });
+            const data = new FormData(this);
+            let object = [];
+
+            data.forEach((value, key) => {
+                object[key] = value;
+            });
+
+            ipc.send("fromDataMessage", object);
+            ipc.send("startMessage");
+
+            return false;
+        });
+
+    console.log(store.get('settings'));
+
+    if (settingsButton)
+        settingsButton.addEventListener('click', function (e) {
+            ipc.send("showSettings");
+        });
+
+    if (settingsForm)
+        settingsForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const data = new FormData(this);
+            let object = {};
+
+            data.forEach((value, key) => {
+                object[key] = value;
+            });
+
+            store.set('settings', object);
+
+            ipc.send("saveSettings");
+
+            return false;
+        });
 
     ipc.on('asyncReply', (event, args) => {
+        if ( ! logs)
+            return;
+
         logs.innerHTML += "\n" + args;
         logs.scrollTo(0, logs.scrollHeight);
     });
